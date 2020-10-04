bc837c6d338cb3375949be4fffd4bfd30f9f84ae
Davi Ps <davipires_souza@hotmail.com>
2020-10-04 12:17:04 -0300

    Site de teste

---
+++ testeSeuMenu/js/main.js
@@ -0,0 +1,22 @@
+
+$('.slider-principal').slick({
+dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000
+
+});
+
+$('.slider-2').slick({
+dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000
+
+});
